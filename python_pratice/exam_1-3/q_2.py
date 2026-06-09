2fd486d025f152cbe10f1bf86dd6b8dee2908e10
sanjeevmagar-git <sanjeevmagarofficail.com>
2026-06-09 15:47:50 +0300

    test

--- python_pratice/exam_1-3/q_2.py
+++ python_pratice/exam_1-3/q_2.py
@@ -1,5 +1,6 @@
 # Here is my code:
-
+# 1. Ask the user to input their first name, last name and favourite number.
+# 2. Print a message to the user that says "Hello <first name> <last name>! Your favourite number is: <favourite number>"
 first = input("Enter your First Name: ")
 last = input("Enter your Last Name: ")
 favourite = int(input("Enter your Favourite Number: "))
